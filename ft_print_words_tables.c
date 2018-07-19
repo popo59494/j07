9ca9d04197d38da00c98ff8a3a6070ad6c236d00
macbook <macbook@MacBook-Pro-de-macbook-2.local>
2018-07-20 01:52:08 +0200

    ex05

---
+++ ft_print_words_tables.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+char	**ft_split_whitespaces(char *str);
+
+void	ft_putchar(char c);
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_print_words_tables(char **tab)
+{
+	int i;
+
+	i = 0;
+	while (tab[i] != NULL)
+	{
+		ft_putstr(tab[i]);
+		ft_putchar('\n');
+		i++;
+	}
+}
