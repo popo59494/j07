1a210f61c767ad54a35b5e658a2ee3e64edfc6be
Clément FERNANDEZ <clement.frndz@gmail.com>
2018-07-22 01:57:00 +0200

    corr

--- j07-void/ex02/ft_ultimate_range.c
+++ j07-void/ex02/ft_ultimate_range.c
@@ -6,11 +6,10 @@
 /*   By: apogorze <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 18:41:16 by apogorze          #+#    #+#             */
-/*   Updated: 2018/07/16 19:00:35 by apogorze         ###   ########.fr       */
+/*   Updated: 2018/07/22 01:56:47 by clement          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
 
 int		ft_ultimate_range(int **range, int min, int max)
@@ -21,26 +20,17 @@ int		ft_ultimate_range(int **range, int min, int max)
 	i = 0;
 	nbvalues = max - min;
 	*range = (int*)malloc(sizeof(int) * (nbvalues));
-	if (max <= min)
-		return ((*range)[0] = 0);
+	if (*range == NULL)
+		return (0);
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
 	while (i < max - min)
 	{
 		(*range)[i] = min + i;
 		i++;
 	}
-	return (**range);
+	return (max - min);
 }
-
-int		main(void)
-{
-	int i;
-	int *str;
-
-	i = -1;
-	ft_ultimate_range(&str, 1, 10);
-	while (++i < 9)
-		printf("\n%d\n", str[i]);
-	return (0);
-}
-			
-
