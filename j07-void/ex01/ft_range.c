537d9940c912e9b5066db1ed4f8d015c430753f0
Clément FERNANDEZ <clement.frndz@gmail.com>
2018-07-22 01:44:19 +0200

    01

--- j07-void/ex01/ft_range.c
+++ j07-void/ex01/ft_range.c
@@ -6,10 +6,9 @@
 /*   By: apogorze <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 16:13:40 by apogorze          #+#    #+#             */
-/*   Updated: 2018/07/16 18:05:05 by apogorze         ###   ########.fr       */
+/*   Updated: 2018/07/22 01:43:52 by clement          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <stdlib.h>
 
 int		*ft_range(int min, int max)
@@ -21,8 +20,10 @@ int		*ft_range(int min, int max)
 	i = 0;
 	nbvalues = max - min;
 	str = (int*)malloc(sizeof(int) * (nbvalues));
-	if (max <= min)
-		return (0);
+	if (str == 0)
+		return (NULL);
+	if (min >= max)
+		return (NULL);
 	while (i < max - min)
 	{
 		str[i] = min + i;
