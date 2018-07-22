4fe52cd1acfe33762cbb560e2303fef29e39cfcf
Clément FERNANDEZ <clement.frndz@gmail.com>
2018-07-22 02:26:57 +0200

    corr

--- j07-void/ex03/ft_concat_params.c
+++ j07-void/ex03/ft_concat_params.c
@@ -6,13 +6,13 @@
 /*   By: apogorze <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 19:10:05 by apogorze          #+#    #+#             */
-/*   Updated: 2018/07/16 19:38:08 by apogorze         ###   ########.fr       */
+/*   Updated: 2018/07/22 02:26:47 by clement          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int		ft_strlen(char *str);
+int		ft_strlen(char *str)
 {
 	int i;
 	
@@ -22,17 +22,43 @@ int		ft_strlen(char *str);
 	return (i);
 } 
 
+char	*ft_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i+j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\n';
+	return (dest);
+}
+
 char	*ft_concat_params(int argc, char **argv)
 {
-	int *str;
+	char *str;
+	int length;
 	int i;
-
+	int j;
+	
 	i = 1;
-	str = (char*)malloc(sizeof(char) * (**argv - *argv[0]);
-	while (i <= argc)
+	j = 1;
+	while (i < argc)
 	{
-		str = argv[i] + '\n';
+		length = length + ft_strlen(argv[i]);
 		i++;
+	}
+	str = (char*)malloc(sizeof(char) * length + 1);
+	while (j < argc)
+	{
+		str = ft_strcat(str, argv[j]);
+		j++;
 	}
 	return (str);
 }
